632f3e030ebb0155bbf0bd3020743fd68a89dbb4
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2025-04-09 15:36:10 +0530

    deployment

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,7 @@ const Groq = require('groq-sdk');
 app.use(express.json());
 
 app.use(cors({
-    origin: `http://localhost:5173`,
+    origin: `${process.env.FRONTEND_URL}`,
     credentials: true
 }));
 
